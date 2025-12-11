c5fd168511db869bf37fd2cc4361e10635ae6239
Antoine Hoarau <703240+ahoarau@users.noreply.github.com>
2025-12-11 16:30:55 +0100

    add new options COAL_BUILD_WITH_LOGGING, QHULL and OCTOMAP

--- development/scripts/pixi/activation.bat
+++ development/scripts/pixi/activation.bat
@@ -5,4 +5,6 @@ for /f "tokens=*" %%i in ('python -c "import sys; print(sys.executable)"') do se
 if not defined COAL_BUILD_TYPE (set COAL_BUILD_TYPE=Release)
 if not defined COAL_PYTHON_STUBS (set COAL_PYTHON_STUBS=ON)
 if not defined COAL_PYTHON_NANOBIND (set COAL_PYTHON_NANOBIND=ON)
-if not defined COAL_HAS_QHULL (set COAL_HAS_QHULL=OFF)
+if not defined COAL_BUILD_WITH_LOGGING (set COAL_BUILD_WITH_LOGGING=OFF)
+if not defined COAL_BUILD_WITH_QHULL (set COAL_BUILD_WITH_QHULL=OFF)
+if not defined COAL_BUILD_WITH_OCTOMAP (set COAL_BUILD_WITH_OCTOMAP=OFF)
